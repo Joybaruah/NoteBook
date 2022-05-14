a93f7b939ff47752af9061faf06a041f6305e6e2
Joy <hoyyun1@gmail.com>
2022-05-15 02:39:54 +0530

    fix link

--- src/context/NoteState.js
+++ src/context/NoteState.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import NoteContext from "./noteContext"
 
 const NoteState = (props)=> {
-    const host = "https://notebook-joy.herokuapp.com/"
+    const host = "https://notebook-joy.herokuapp.com"
     const notesInitial = []
 
     const [notes, setNotes] = useState(notesInitial)
